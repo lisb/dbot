6301bdb8d45cc98470350bafe4df4002fc3a0afd
hayami8 <109721124+hayami8@users.noreply.github.com>
2025-05-22 13:46:26 +0900

    refactor: onfileを単純化

--- scripts/ping.js
+++ scripts/ping.js
@@ -12,51 +12,42 @@ const { generateBotResponse } = require("./bot");
 
 const g_messages = [];
 
+const onfile = (res, file) => {
+  res.send("ファイルの検証が終わるまで少々お待ちください...");
+  const dl = () => {
+    res.download(file, async (path, err) => {
+      if (err) {
+        console.log("failed2:" + err.message);
+        setTimeout(dl, 30000);
+      }
+      console.log("succeed2:" + path);
+      const message = {
+        role: "user",
+        content: res.json.text ? res.json.text.replace("Hubot ", "") : "",
+        images: [path],
+      };
+      g_messages.push(message);
+      const command = "c_message";
+      const response = await generateBotResponse(command, [message]);
+      if (!response) {
+        return;
+      }
+      g_messages.push({
+        role: "assistant",
+        content: response,
+      });
+
+      res.send(response);
+    });
+  };
+  setTimeout(dl, 30000);
+};
+
 module.exports = (robot) => {
   robot.hear(/room/i, (res) => {
     // この部分
     res.send(`This room id is ${res.message.room}`);
   });
-  const onfile = (res, file) => {
-    res.send(
-      [
-        "File received.",
-        `name: ${file.name}`,
-        `type: ${file.content_type}`,
-        `size: ${file.content_size}bytes`,
-      ].join("\n")
-    );
-    res.send("ファイルの検証が終わるまで少々お待ちください...");
-    const dl = () => {
-      res.download(file, (path, err) => {
-        if (err) {
-          console.log("failed2:" + err.message);
-          setTimeout(dl, 30000);
-        } else {
-          console.log("succeed2:" + path);
-          const message = {
-            role: "user",
-            content: res.json.text ? res.json.text.replace("Hubot ", "") : "",
-            images: [path],
-          };
-          g_messages.push(message);
-          const command = "c_message";
-          generateBotResponse(command, [message]).then((response) => {
-            if (!response) {
-              return;
-            }
-            g_messages.push({
-              role: "assistant",
-              content: response,
-            });
-
-            res.send(response);
-          });
-        }
-      });
-    };
-    setTimeout(dl, 30000);
-  };
 
   // ファイルが1つだけの場合
   robot.hear("file", (res) => {
